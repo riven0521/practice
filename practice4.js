c301ccae1a7047c97ef4fc7298a80a973f48313e
CyberRim <cyberrim@foxmail.com>
2021-08-02 22:56:59 +0800

    青蛙跳台阶

---
+++ practice4.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+// 一个青蛙一次可以跳1个台阶或者2个台阶，输入台阶n，请问调上n个台阶一共有几种不同的跳法
+// 返回跳法的数量
+function test(n) {
+	/*在这里写代码*/
+
+
+	/*在这里写代码*/
+}
+var result = test(n);
+console.log(result);

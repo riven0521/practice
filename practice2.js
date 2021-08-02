64bb37baf58fb4ec8dca440c400651565def542e
riven0521 <275291412@qq.com>
2021-08-02 21:59:00 +0800

    1

--- practice2.js
+++ practice2.js
@@ -1,9 +1,18 @@
 //冒泡排序
 function test(array) {
-	/*在这里写代码*/
-
-
-	/*在这里写代码*/
+	// /*在这里写代码*/
+	for (j = 0; j < array.length - 1; j++) {
+		for (i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1]) {
+				var c;
+				c = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = c;
+				/*在这里写代码*/
+			}
+		}
+	}
+	return array;
 }
-result = test([5,6,7,4,5,2,8,11,1]);
+result = test([5, 6, 7, 4, 2, 8, 11, 1]);
 console.log(result);

bbd6c7ed6f0cbe6e66e9c95425cb5fc356b09fab
riven0521 <275291412@qq.com>
2021-08-05 00:32:35 +0800

    1

--- leetcode/9.js
+++ leetcode/9.js
@@ -1,15 +1,26 @@
 /*jshint esversion: 6 */
 function test(x) {
 	'use strict';
+	var z = 0;
+	var y = x;
 	let x1 = x;
-	let i = 0;
-	while (x != 0) {
+	var i = 0;
+	while (x1 != 0) {
 		x1 = Math.floor(x1 / 10);
 		i++;
 	}
 	console.log(i);
+	for (var j = 0; j < i; j++) {
+		z = z * 10 + x % 10;
+		x = Math.floor(x / 10);
+		if (z == y) {
+			return true;
+		} 
+	}
+	return false;
 }
 
 
-var result = test(121);
+
+var result = test(12321);
 console.log(result);

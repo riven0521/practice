cd06488531793d04336d3add1b43746888e10d2b
CyberRim <cyberrim@foxmail.com>
2021-08-06 05:28:47 +0800

    14题代码

--- leetcode/14.js
+++ leetcode/14.js
@@ -3,11 +3,49 @@
 function test(strs) {
 	/*把下面代码粘到网站的函数内*/
 	'use strict';
+	if (strs.length == 1) {
+		return strs[0];
+	}
+	let minStr = '';
+	let charCheck = '';
+	let isEnd = false;
+	for (let i = 0; i < strs[0].length; i++) {
+		for (let j = 0; j < strs.length; j++) {
+			let strNow = strs[j];
 
+			if (i >= strNow.length) {
+				if (j != 0) {
+					isEnd = true;
+				}
+				break;
+			}
+			let charNow = strNow[i];
 
+			if (j == 0) {
+				charCheck = charNow;
+				minStr += charCheck;
+				continue;
+			}
+
+			if (charNow != charCheck) {
+				isEnd = true;
+				break;
+			}
+		}
+		if (isEnd) {
+			break;
+		}
+
+	}
+	if (isEnd) {
+		minStr = minStr.substring(0, minStr.length - 1);
+
+	}
+	return minStr;
 	/*************************/
+
 }
 
 //传递的参数就是测试案例
-var result = test(["flower", "flow", "flight"]);
+var result = test(["flower", "flower", "flower", "flower"]);
 console.log(result);
--- leetcode/9.js
+++ leetcode/9.js
@@ -1,26 +1,15 @@
 /*jshint esversion: 6 */
 function test(x) {
 	'use strict';
-	var z = 0;
-	var y = x;
 	let x1 = x;
-	var i = 0;
-	while (x1 != 0) {
+	let i = 0;
+	while (x != 0) {
 		x1 = Math.floor(x1 / 10);
 		i++;
 	}
 	console.log(i);
-	for (var j = 0; j < i; j++) {
-		z = z * 10 + x % 10;
-		x = Math.floor(x / 10);
-		if (z == y) {
-			return true;
-		} 
-	}
-	return false;
 }
 
 
-
-var result = test(12321);
+var result = test(121);
 console.log(result);

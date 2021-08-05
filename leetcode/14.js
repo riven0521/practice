75f2d8a119f4871c59820ecc5a47e15e39e61cf7
CyberRim <cyberrim@foxmail.com>
2021-08-06 03:08:29 +0800

    改14题模板

--- leetcode/14.js
+++ leetcode/14.js
@@ -1,7 +1,13 @@
 /*jshint esversion: 6 */
+// 参数名跟题目改成一致
 function test(strs) {
+	/*把下面代码粘到网站的函数内*/
 	'use strict';
 
-	var result = test(strs = ["flower", "flow", "flight"]);
-	console.log();
+
+	/*************************/
 }
+
+//传递的参数就是测试案例
+var result = test(["flower", "flow", "flight"]);
+console.log(result);

561bc4915dd4ce7d5e9722d646bc521e7188dc63
riven0521 <275291412@qq.com>
2021-08-07 23:50:41 +0800

    力扣第14题

--- leetcode/14.js
+++ leetcode/14.js
@@ -1,87 +1,44 @@
 /*jshint esversion: 6 */
 // 参数名跟题目改成一致
 function test(strs) {
-	/*把下面代码粘到网站的函数内*/
-	'use strict';
-	if (strs.length == 1) {
-		return strs[0];
-	}
-	let minStr = '';
-	let charCheck = '';
-	let isEnd = false;
-	for (let i = 0; i < strs[0].length; i++) {
-		for (let j = 0; j < strs.length; j++) {
-			let strNow = strs[j];
-
-			if (i >= strNow.length) {
-				if (j != 0) {
-					isEnd = true;
-				}
-				break;
-			}
-			let charNow = strNow[i];
-
-			if (j == 0) {
-				charCheck = charNow;
-				minStr += charCheck;
-				continue;
-			}
-
-			if (charNow != charCheck) {
-				isEnd = true;
-				break;
-			}
-		}
-		if (isEnd) {
-			break;
-		}
-
-	}
-	if (isEnd) {
-		minStr = minStr.substring(0, minStr.length - 1);
-
-	}
-	return minStr;
-	/*************************/
-
+    /*把下面代码粘到网站的函数内*/
+    'use strict';
+    if (strs.length == 1) { //判断是否为空
+        return strs;
+    }
+    let charCheck = '';
+    let minStr = '';
+    let end = false;
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 0; j < strs.length; j++) { //比较第一个字符
+            let strNow = strs[j];
+            if (i >= strNow.length) {
+                if (j != 0) {
+                    end = true;
+                }
+                break;
+            }
+            let charNow = strNow[i];
+            if (j == 0) {
+                charCheck = charNow;
+                minStr += charCheck;
+                continue; //中断迭代；
+            }
+            if (charNow != charCheck) {
+                end = true;
+                break;
+            }
+        } //结束j循环
+        if (end) {
+            break;
+        }
+    } //结束i循环
+    if (end) {
+        minStr = minStr.substring(0, minStr.length - 1);
+    }
+    return minStr;
 }
 
 //传递的参数就是测试案例
 var result = test(["flower", "flower", "flower", "flower"]);
 console.log(result);
-
-
-
-
-
-// // 参数名跟题目改成一致
-// function test(strs) {
-// 	/*把下面代码粘到网站的函数内*/
-// 	'use strict';
-// var str = '';
-// 	var str1 = '';
-// 	for (var i = 0; i < strs.length; i++) {
-// 		if (strs[0][i] == strs[1][i] && strs[0][i] == strs[2][i]) {
-// 			str += strs[0][i];
-// 		}
-
-// 	}
-// 	if (str == '') {
-// 		return str1;
-// 	} else {
-// 		return str;
-// 	}
-// 	// return str;
-// 	// if (strs[0][0] == strs[1][0] && strs[0][0] == strs[2][0]) {
-// 	// 	str = strs[0][0];
-// 	// }
-// 	// if (strs[0][1] == strs[1][1] && strs[0][1] == strs[2][1]) {
-// 	// 	str = strs[0][0]+strs[0][1];
-// 	// }
-
-// 	// return str;
-
-// 	//传递的参数就是测试案例
-// }
-// var result = test(["a1dog", "a1racecar", "a1car"]);
-// console.log(result);

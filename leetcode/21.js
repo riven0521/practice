0f58f53834db719166232df6ac1b0914004f7164
CyberRim <cyberrim@foxmail.com>
2021-08-09 17:00:56 +0800

    增加21题的测试代码

--- leetcode/21.js
+++ leetcode/21.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 function test(l1, l2) {
 
     if (l1 === null) {
@@ -16,5 +17,45 @@ function test(l1, l2) {
 
 
 }
-var result = test([1, 2, 4], [1, 3, 4]);
+
+/***********自己改这两个数组********/
+const arr1 = [1, 2, 4];
+const arr2 = [1, 3, 4];
+/*******************************/
+
+
+/********用上面两数组转换成两链表l1和l2*********/
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
+function arrToList(arr) {
+    let lH = null;
+    let lT = lH;
+    arr.forEach((n) => {
+        const node = new ListNode(n);
+        if (lH == null) {
+            lH = node;
+            lT = lH;
+        } else {
+            lT.next = node;
+            lT = lT.next;
+        }
+    });
+    return lH;
+}
+function listToArr(node){
+    const arr = [];
+    while(node!=null){
+        arr.push(node.val);
+        node = node.next;
+    }
+    return arr;
+}
+const l1 = arrToList(arr1);
+const l2 = arrToList(arr2);
+/*******************************/
+
+const result = listToArr(test(l1, l2));
 console.log(result);

a12c8e71d91890041d37862e4de51cc88f5733cc
riven0521 <275291412@qq.com>
2021-08-11 05:24:54 +0800

    27mmp

--- leetcode/27.js
+++ leetcode/27.js
@@ -1,6 +1,12 @@
 /*jshint esversion: 6 */
 function text(nums, val) {
 	'use strict';
+	if (nums.length == 1) {
+		if (nums[0] == val) {
+			return 0;
+		}
+	}
+
 	let i = 0;
 	var temp = 0;
 	var z = nums.length - 1;
@@ -26,33 +32,34 @@ function text(nums, val) {
 		}
 
 	}
+	console.log(nums);
 	return z + 1;
 }
 
 
 
-var result = text([3, 2, 1, 4, 3, 5, 7], 3);
+var result = text([4, 5], 4);
 console.log(result);
 
-function textOfCyberRim(nums, val) {
-	'use strict';
-	let l = 0;
-	let r = nums.length - 1;
-	while (r >= 0 && nums[r] == val) r--;
-	while (l < r) {
-		if (nums[l] == val) {
-			let t = nums[r];
-			nums[r] = nums[l];
-			nums[l] = t;
-			while (r >= 0 && nums[r] == val) r--;
-		}
-		l++;
-	}
-	return r == -1 ? 0 : r + 1;
-	/*上面这个叫三元运算符
-	 *条件?值1:值2
-	 *如果条件为真那值为值1 
-     *如果条件为假那值为值2
-     *你需要去搜了解下
-	*/
-}
+// function textOfCyberRim(nums, val) {
+// 	'use strict';
+// 	let l = 0;
+// 	let r = nums.length - 1;
+// 	while (r >= 0 && nums[r] == val) r--;
+// 	while (l < r) {
+// 		if (nums[l] == val) {
+// 			let t = nums[r];
+// 			nums[r] = nums[l];
+// 			nums[l] = t;
+// 			while (r >= 0 && nums[r] == val) r--;
+// 		}
+// 		l++;
+// 	}
+// 	return r == -1 ? 0 : r + 1;
+// 	/*上面这个叫三元运算符
+// 	 *条件?值1:值2
+// 	 *如果条件为真那值为值1 
+// 	 *如果条件为假那值为值2
+// 	 *你需要去搜了解下
+// 	 */
+// }

5b222bca873e49170c756994d539dbd184db3506
CyberRim <cyberrim@foxmail.com>
2021-08-11 04:27:55 +0800

    增加27题实例代码

--- leetcode/27.js
+++ leetcode/27.js
@@ -26,10 +26,33 @@ function text(nums, val) {
 		}
 
 	}
-	return nums;
+	return z + 1;
 }
 
 
 
 var result = text([3, 2, 1, 4, 3, 5, 7], 3);
 console.log(result);
+
+function textOfCyberRim(nums, val) {
+	'use strict';
+	let l = 0;
+	let r = nums.length - 1;
+	while (r >= 0 && nums[r] == val) r--;
+	while (l < r) {
+		if (nums[l] == val) {
+			let t = nums[r];
+			nums[r] = nums[l];
+			nums[l] = t;
+			while (r >= 0 && nums[r] == val) r--;
+		}
+		l++;
+	}
+	return r == -1 ? 0 : r + 1;
+	/*上面这个叫三元运算符
+	 *条件?值1:值2
+	 *如果条件为真那值为值1 
+     *如果条件为假那值为值2
+     *你需要去搜了解下
+	*/
+}
